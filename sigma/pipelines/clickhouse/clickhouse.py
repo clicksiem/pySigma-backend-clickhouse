5c5a7904d7ce802bd3ec3c579dd09e83f83befdc
souzo <me@souzo.me>
2026-05-22 20:29:08 -0300

    chore: add base clicksiem pipeline

--- sigma/pipelines/clickhouse/clickhouse.py
+++ sigma/pipelines/clickhouse/clickhouse.py
@@ -4,18 +4,28 @@ from sigma.pipelines.common import (
     logsource_windows,
     windows_logsource_mapping,
 )
-from sigma.pipelines.base import Pipeline, ProcessingPipeline
+from sigma.pipelines.base import ProcessingPipeline
 from sigma.processing.transformations import FieldMappingTransformation
 
-def clickhouse_wazuh_pipeline() -> ProcessingPipeline:
+clicksiem_mapping: dict[str | None, str | list[str]] = {
+    "EventID": "alert.data.win.system.eventID",
+    "Channel": "alert.data.win.system.channel",
+    "ComputerName": "alert.data.win.system.computer",
+    "Image": "data.win.eventdata.image",
+    "SubjectUserName": "alert.data.win.eventdata.subjectUserName",
+    "TargetUserName": "alert.data.win.eventdata.targetUserName",
+}
+
+
+def clickhouse_clicksiem_pipeline() -> ProcessingPipeline:
     return ProcessingPipeline(
-        name="clickhouse wazuh pipeline",
+        name="clickhouse clicksiem pipeline",
         allowed_backends=frozenset(),
         priority=20,
         items=[
             ProcessingItem(
                 identifier="clickhouse_wazuh_fieldmapping",
-                transformation=FieldMappingTransformation({"test": ""}),
+                transformation=FieldMappingTransformation(clicksiem_mapping),
             )
         ],
     )
