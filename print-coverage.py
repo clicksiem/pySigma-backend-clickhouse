3bc9ddbcdbd4981fda8ca1a063b47a5e46f84485
souzo <me@souzo.me>
2026-05-22 20:33:44 -0300

    ci: add tests and coverage to CI

---
+++ print-coverage.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as et
+
+tree = et.parse("cov.xml")
+root = tree.getroot()
+coverage = float(root.attrib["line-rate"]) * 100
+print(f"COVERAGE={coverage:3.4}%")
+if coverage >= 95.0:
+    print("COVERAGE_COLOR=green")
+elif coverage >= 90.0:
+    print("COVERAGE_COLOR=yellow")
+elif coverage >= 85.0:
+    print("COVERAGE_COLOR=orange")
+else:
+    print("COVERAGE_COLOR=red")
